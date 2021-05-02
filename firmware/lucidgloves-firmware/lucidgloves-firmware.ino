885c4c013b4829dc960224c5f826e854b9e37488
Lucas LucidVR <35583218+lucas-vrtech@users.noreply.github.com>
2021-05-01 20:33:36 -0400

    Add different pin configurations for Arduino/ESP32 (Suggested by Jakub Nagy)

--- firmware/lucidgloves-firmware/lucidgloves-firmware.ino
+++ firmware/lucidgloves-firmware/lucidgloves-firmware.ino
@@ -5,7 +5,6 @@
  */
 //This is the configuration file, main structure in _main.ino
 //CONFIGURATION SETTINGS:
-#define DEBUG_LED LED_BUILTIN
 #define COMMUNICATION COMM_SERIAL //Which communication protocol to use
 
 #define LOOP_TIME 4 //How much time between data sends (ms), set to 0 for a good time :)
@@ -17,24 +16,8 @@
 #define COMM_BTSERIAL 1 //serial over Bluetooth (Work in progress)
   #define BTSERIAL_DEVICE_NAME "ESP32test"
 
-//PINS CONFIGURATION 
-//(This configuration is for Arduino Nano so make sure to change if you're on another board)
-#define PIN_PINKY     A0
-#define PIN_RING      A1
-#define PIN_MIDDLE    A2
-#define PIN_INDEX     A3
-#define PIN_THUMB     A4
-#define PIN_JOY_X     A6
-#define PIN_JOY_Y     A7
-#define PIN_JOY_BTN   7 
-#define PIN_A_BTN     8 
-#define PIN_B_BTN     9
-#define PIN_TRIG_BTN  10 //unused if gesture set
-#define PIN_GRAB_BTN  11 //unused if gesture set
-#define PIN_PNCH_BTN  12 //unused if gesture set
-
 //ANALOG INPUT CONFIG
-#define ANALOG_MAX 1023   //Set to 1023 on Arduino, 4095 on ESP32
+#define ANALOG_MAX 1023   //Set to 1023 on Arduino, 4095 on ESP32. Make sure is the same as driver setting.
 #define FLIP_POTS  false  //Flip values from potentiometers (for fingers!) if they are backwards
 
 //Gesture enables, make false to use button override
@@ -48,3 +31,38 @@
 #define JOY_FLIP_Y false
 
 #define NO_THUMB false //If for some reason you don't want to track the thumb
+
+//PINS CONFIGURATION 
+#if defined(__AVR__)
+  //(This configuration is for Arduino Nano so make sure to change if you're on another board)
+  #define PIN_PINKY     A0
+  #define PIN_RING      A1
+  #define PIN_MIDDLE    A2
+  #define PIN_INDEX     A3
+  #define PIN_THUMB     A4
+  #define PIN_JOY_X     A6
+  #define PIN_JOY_Y     A7
+  #define PIN_JOY_BTN   7 
+  #define PIN_A_BTN     8 
+  #define PIN_B_BTN     9
+  #define PIN_TRIG_BTN  10 //unused if gesture set
+  #define PIN_GRAB_BTN  11 //unused if gesture set
+  #define PIN_PNCH_BTN  12 //unused if gesture set
+  #define DEBUG_LED LED_BUILTIN
+#elif defined(ESP32)
+  //(This configuration is for ESP32 DOIT V1 so make sure to change if you're on another board)
+  #define PIN_PINKY     36
+  #define PIN_RING      39
+  #define PIN_MIDDLE    34
+  #define PIN_INDEX     35
+  #define PIN_THUMB     32
+  #define PIN_JOY_X     33
+  #define PIN_JOY_Y     25
+  #define PIN_JOY_BTN   26 
+  #define PIN_A_BTN     27 
+  #define PIN_B_BTN     14
+  #define PIN_TRIG_BTN  12 //unused if gesture set
+  #define PIN_GRAB_BTN  13 //unused if gesture set
+  #define PIN_PNCH_BTN  23 //unused if gesture set
+  #define DEBUG_LED LED_BUILTIN
+#endif
