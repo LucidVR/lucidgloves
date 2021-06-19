5177de438aec571b01886bc931583a35cf1b6efc
Lucas LucidVR <35583218+lucas-vrtech@users.noreply.github.com>
2021-06-19 19:47:34 -0400

    Add legacy decoding

--- firmware/lucidgloves-firmware/Encoding.ino
+++ firmware/lucidgloves-firmware/Encoding.ino
@@ -1,10 +1,42 @@
+/*struct inputData {
+  int* flexion;
+  int joyX;
+  int joyY;
+  bool joyClick;
+  bool triggerButton;
+  bool aButton;
+  bool bButton;
+  bool grab;
+  bool pinch;
+};
+
+struct outputData{
+  int* hapticLimits;
+};
+*/
+
+//legacy encoding
 char* encode(int* flexion, int joyX, int joyY, bool joyClick, bool triggerButton, bool aButton, bool bButton, bool grab, bool pinch){
-  static char stringToPrint[75];
+  static char stringToEncode[75];
   
-  sprintf(stringToPrint, "%d&%d&%d&%d&%d&%d&%d&%d&%d&%d&%d&%d&%d\n", 
+  sprintf(stringToEncode, "%d&%d&%d&%d&%d&%d&%d&%d&%d&%d&%d&%d&%d\n", 
   flexion[4], flexion[3], flexion[2], flexion[1], flexion[0],
   joyX, joyY, joyClick,
   triggerButton, aButton, bButton, grab, pinch
   );
-  return stringToPrint;
+  return stringToEncode;
+}
+//legacy decoding
+int* decodeData(char* stringToDecode){
+    int* hapticLimits;
+    byte index = 0;
+    char* ptr = strtok(stringToDecode, "&");  // takes a list of delimiters
+    while(ptr != NULL)
+    {
+        hapticLimits[index] = atoi(ptr);
+        index++;
+        ptr = strtok(NULL, "&");  // takes a list of delimiters
+    }
+
+    return hapticLimits;
 }
