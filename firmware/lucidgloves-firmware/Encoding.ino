8bdf255629fc73b2a97c435063346d8c7405740b
Lucas LucidVR <35583218+lucas-vrtech@users.noreply.github.com>
2021-07-03 09:07:09 -0400

    Add alpha encoding

--- firmware/lucidgloves-firmware/Encoding.ino
+++ firmware/lucidgloves-firmware/Encoding.ino
@@ -15,8 +15,9 @@ struct outputData{
 };
 */
 
+#if ENCODING == ENCODING_LEGACY //will be moved to its own manager in Firmware v4
 //legacy encoding
-char* encode(int* flexion, int joyX, int joyY, bool joyClick, bool triggerButton, bool aButton, bool bButton, bool grab, bool pinch){
+char* encode(int* flexion, int joyX, int joyY, bool joyClick, bool triggerButton, bool aButton, bool bButton, bool grab, bool pinch, bool calib){
   static char stringToEncode[75];
   
   sprintf(stringToEncode, "%d&%d&%d&%d&%d&%d&%d&%d&%d&%d&%d&%d&%d\n", 
@@ -37,3 +38,18 @@ void decodeData(char* stringToDecode, int* hapticLimits){
         ptr = strtok(NULL, "&");  // takes a list of delimiters
     }
 }
+#endif
+
+#if ENCODING == ENCODE_ALPHA
+//alphabetic encoding
+char* encode(int* flexion, int joyX, int joyY, bool joyClick, bool triggerButton, bool aButton, bool bButton, bool grab, bool pinch, bool calib){
+  static char stringToEncode[75];
+  
+  sprintf(stringToEncode, "A%dB%dC%dD%dE%dF%dG%d%s%s%s%s%s%s%s\n", 
+  flexion[4], flexion[3], flexion[2], flexion[1], flexion[0],
+  joyX, joyY, joyClick?"H":"",
+  triggerButton?"I":"", aButton?"J":"", bButton?"K":"", grab?"L":"", pinch?"M":"", calib?"O":""
+  );
+  return stringToEncode;
+}
+#endif
