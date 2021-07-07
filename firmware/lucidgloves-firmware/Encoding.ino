645536b4516ac0ddfd5d0d6e726c342877d0bc63
Lucas LucidVR <35583218+lucas-vrtech@users.noreply.github.com>
2021-07-07 14:50:00 -0400

    Add alpha decoding

--- firmware/lucidgloves-firmware/Encoding.ino
+++ firmware/lucidgloves-firmware/Encoding.ino
@@ -45,11 +45,28 @@ void decodeData(char* stringToDecode, int* hapticLimits){
 char* encode(int* flexion, int joyX, int joyY, bool joyClick, bool triggerButton, bool aButton, bool bButton, bool grab, bool pinch, bool calib){
   static char stringToEncode[75];
   
-  sprintf(stringToEncode, "A%dB%dC%dD%dE%dF%dG%d%s%s%s%s%s%s%s\n", 
   flexion[4], flexion[3], flexion[2], flexion[1], flexion[0],
   joyX, joyY, joyClick?"H":"",
   triggerButton?"I":"", aButton?"J":"", bButton?"K":"", grab?"L":"", pinch?"M":"", calib?"O":""
   );
   return stringToEncode;
 }
+
+//alphabetic decoding
+void decodeData(char* stringToDecode, int* hapticLimits){
+  hapticLimits[0] = getArgument(stringToDecode, 'A'); //thumb
+  hapticLimits[1] = getArgument(stringToDecode, 'B'); //index
+  hapticLimits[2] = getArgument(stringToDecode, 'C'); //middle
+  hapticLimits[3] = getArgument(stringToDecode, 'D'); //ring
+  hapticLimits[4] = getArgument(stringToDecode, 'E'); //pinky
+}
+
+int getArgument(char* stringToDecode, char command){
+  char* start = strchr(stringToDecode, command);
+  if (start == NULL)
+    return -1;
+  else
+    return atoi(start + 1);
+}
+
 #endif
