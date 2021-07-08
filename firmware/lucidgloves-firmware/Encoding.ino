43e9532e388843f78696cde0f424599aabf49bc5
Lucas LucidVR <35583218+lucas-vrtech@users.noreply.github.com>
2021-07-07 20:26:14 -0400

    Fix encoding

--- firmware/lucidgloves-firmware/Encoding.ino
+++ firmware/lucidgloves-firmware/Encoding.ino
@@ -45,6 +45,7 @@ void decodeData(char* stringToDecode, int* hapticLimits){
 char* encode(int* flexion, int joyX, int joyY, bool joyClick, bool triggerButton, bool aButton, bool bButton, bool grab, bool pinch, bool calib){
   static char stringToEncode[75];
   
+  sprintf(stringToEncode, "A%dB%dC%dD%dE%dF%dG%d%s%s%s%s%s%s%s\n", 
   flexion[4], flexion[3], flexion[2], flexion[1], flexion[0],
   joyX, joyY, joyClick?"H":"",
   triggerButton?"I":"", aButton?"J":"", bButton?"K":"", grab?"L":"", pinch?"M":"", calib?"O":""
@@ -52,13 +53,14 @@ char* encode(int* flexion, int joyX, int joyY, bool joyClick, bool triggerButton
   return stringToEncode;
 }
 
-//alphabetic decoding
+//legacy decoding
 void decodeData(char* stringToDecode, int* hapticLimits){
   hapticLimits[0] = getArgument(stringToDecode, 'A'); //thumb
   hapticLimits[1] = getArgument(stringToDecode, 'B'); //index
   hapticLimits[2] = getArgument(stringToDecode, 'C'); //middle
   hapticLimits[3] = getArgument(stringToDecode, 'D'); //ring
   hapticLimits[4] = getArgument(stringToDecode, 'E'); //pinky
+  //Serial.println("Haptic: "+ (String)hapticLimits[0] + " " + (String)hapticLimits[1] + " " + (String)hapticLimits[2] + " " + (String)hapticLimits[3] + " " + (String)hapticLimits[4] + " ");
 }
 
 int getArgument(char* stringToDecode, char command){
