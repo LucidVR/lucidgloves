259300a0c5be1de165b753d327e1907626a295f9
Lucas LucidVR <35583218+lucas-vrtech@users.noreply.github.com>
2021-08-14 23:37:22 -0400

    Gestures now use finger indeces

--- firmware/lucidgloves-firmware/gesture.ino
+++ firmware/lucidgloves-firmware/gesture.ino
@@ -1,11 +1,11 @@
 bool grabGesture(int *flexion){
-  return (flexion[0] + flexion[1] + flexion[2] + flexion[3]) / 4 <= ANALOG_MAX/2 ? 0:1;
+  return (flexion[PINKY_IND] + flexion[RING_IND] + flexion[MIDDLE_IND] + flexion[INDEX_IND]) / 4 <= ANALOG_MAX/2 ? 0:1;
 }
 
 bool pinchGesture(int *flexion){
-  return (flexion[0] + flexion[1]) / 2 <= ANALOG_MAX/2 ? 0:1;
+  return (flexion[INDEX_IND] + flexion[THUMB_IND]) / 2 <= ANALOG_MAX/2 ? 0:1;
 }
 
 bool triggerGesture(int *flexion){
-  return flexion[1]<=(ANALOG_MAX/2)?0:1;
+  return flexion[INDEX_IND]<=(ANALOG_MAX/2)?0:1;
 }
