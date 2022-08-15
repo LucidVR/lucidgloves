ec575f065cbd75a68fcb597907b83d1b8cf26613
Lucas LucidVR <35583218+lucas-vrtech@users.noreply.github.com>
2022-08-15 18:23:58 -0400

    Hardcode speedyread
    Co-Authored-By: danwillm <39023874+danwillm@users.noreply.github.com>

--- firmware/lucidgloves-firmware/input.ino
+++ firmware/lucidgloves-firmware/input.ino
@@ -1,3 +1,4 @@
+#include "esp_adc_cal.h"
 // Requires RunningMedian library by Rob Tillaart
 #if ENABLE_MEDIAN_FILTER
   #include <RunningMedian.h>
@@ -15,6 +16,8 @@
   };
 #endif
 
+#define DEFAULT_VREF 1100
+esp_adc_cal_characteristics_t *adc_chars;
 
 byte selectPins[] = {PINS_MUX_SELECT};
 
@@ -62,6 +65,28 @@ void setupInputs(){
     pinMode(selectPins[i], OUTPUT);
   }
   #endif
+  
+  //Range 0-4096
+  adc1_config_width(ADC_WIDTH_BIT_12);
+  // full voltage range
+  adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_DB_11);
+
+  // check to see what calibration is available
+  if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_VREF) == ESP_OK)
+  {
+    Serial.println("Using voltage ref stored in eFuse");
+  }
+  if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_TP) == ESP_OK)
+  {
+    Serial.println("Using two point values from eFuse");
+  }
+  if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_DEFAULT_VREF) == ESP_OK)
+  {
+    Serial.println("Using default VREF");
+  }
+  //Characterize ADC
+  adc_chars = (esp_adc_cal_characteristics_t *)calloc(1, sizeof(esp_adc_cal_characteristics_t));
+  esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars);
 }
 
 int analogPinRead(int pin){
@@ -185,9 +210,14 @@ int readMux(byte pin){
       break;
   }
   delayMicroseconds(1);
-  return analogRead(MUX_INPUT);
+  //return analogRead(MUX_INPUT);
+  return speedyRead();
 }
 #endif
+
+int speedyRead(){
+  return adc1_get_raw(ADC1_CHANNEL_4);
+}
 
 void getFingerPositions(bool calibrating, bool reset){
   #if FLEXION_MIXING == MIXING_NONE //no mixing, just linear
