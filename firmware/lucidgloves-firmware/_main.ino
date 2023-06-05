7d725b61634679110c7480ffcf642c3d5e467faa
REXXER301 <62205233+REXXER301@users.noreply.github.com>
2023-06-05 19:07:58 +0200

    Update _main.ino

--- firmware/lucidgloves-firmware/_main.ino
+++ firmware/lucidgloves-firmware/_main.ino
@@ -12,6 +12,8 @@ void setup() {
     comm = new SerialCommunication();
   #elif COMMUNICATION == COMM_BTSERIAL
     comm = new BTSerialCommunication();
+  #elif COMMUNICATION == COMM_WIFISERIAL
+    comm = new WIFISerialCommunication();
   #endif  
   comm->start();
 
