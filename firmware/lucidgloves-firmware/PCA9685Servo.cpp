082316e804d8d48c2e7b361d8938656783c73a58
Roastlawyer <88754980+Roastlawyer@users.noreply.github.com>
2023-06-09 05:27:09 -0600

    Seperate PCA initialization from constructor definition Servo::Servo() class
    The previous version of the PCA initialization code was being called before I2C was initialized, was being reran for each of the 5 servo objects, & wasn't acknowledging the configured I2C pins.
    
    This commit redoes it to be run as part of a subfunction within InitServoInterface() just as I2C initialization is.

--- firmware/lucidgloves-firmware/PCA9685Servo.cpp
+++ firmware/lucidgloves-firmware/PCA9685Servo.cpp
@@ -1,13 +1,18 @@
 #pragma once
 #include "PCA9685Servo.hpp"
 
-Servo::Servo()
+Adafruit_PWMServoDriver pwm_board_0 = Adafruit_PWMServoDriver(PWM_Board_0_I2C_ADDRESS, Wire);
+
+int Initialize_PCA9685_Board()
 {
   pwm_board_0.begin();
   pwm_board_0.setOscillatorFrequency(25000000);
   pwm_board_0.setPWMFreq(PWM_Board_0_PWM_FREQUENCY);  // Analog servos usually run at ~50 Hz updates
   Serial.println("PCA9685 Board Initialized ");
 }
+
+Servo::Servo()
+{}
 
 int Servo::attach(int pin)
 {
@@ -29,4 +34,5 @@ void Servo::write(int value)
 
 void Servo::writeMicroseconds(int value){
     pwm_board_0.writeMicroseconds(this->driverChannel, value);
+    //Serial.println("Servo called!");
 }
--- firmware/lucidgloves-firmware/PCA9685Servo.hpp
+++ firmware/lucidgloves-firmware/PCA9685Servo.hpp
@@ -2,6 +2,8 @@
 #include <Adafruit_PWMServoDriver.h>
 #include "AdvancedConfig.h"
 
+int Initialize_PCA9685_Board();
+
 #define MIN_PULSE_WIDTH ServoMin_uS   //Sets the MIN_PULSE_WIDTH setting used by smooth stepping to your configured ServoMin (Default in esp32servo is 500)
 #define MAX_PULSE_WIDTH ServoMax_uS   //Sets the Max_PULSE_WIDTH setting used by smooth stepping to your configured ServoMax (Default in esp32servo is 2500)
 
@@ -17,5 +19,4 @@ private:
   int minMicroSeconds = ServoMin_uS;                  // microsecond value to move servo to 0* or fully retracted position  
   int maxMicroSeconds = ServoMax_uS;                  // microsecond value to move servo to 180* or whatever it's max rotation position is  
   int driverChannel = 0;                          // driverboard channel connected to the servo
-  Adafruit_PWMServoDriver pwm_board_0;
 };
