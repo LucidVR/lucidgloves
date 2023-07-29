e76510a50081ee4ed88bbb1bf66c72e5ad014fd0
Lucas LucidVR <35583218+lucas-vrtech@users.noreply.github.com>
2023-07-29 15:53:39 -0400

    Make gesture a class

--- firmware/lucidgloves-firmware/Gesture.cpp
+++ firmware/lucidgloves-firmware/Gesture.cpp
@@ -1,14 +1,13 @@
-#include "gesture.h"
-#include "AdvancedConfig.h"
+#include "Gesture.h"
 
-bool grabGesture(int *flexion){
+bool Gesture::grabGesture(int *flexion){
   return (flexion[PINKY_IND] + flexion[RING_IND] + flexion[MIDDLE_IND] + flexion[INDEX_IND]) / 4 <= ANALOG_MAX/2 ? 0:1;
 }
 
-bool pinchGesture(int *flexion){
+bool Gesture::pinchGesture(int *flexion){
   return (flexion[INDEX_IND] + flexion[THUMB_IND]) / 2 <= ANALOG_MAX/2 ? 0:1;
 }
 
-bool triggerGesture(int *flexion){
+bool Gesture::triggerGesture(int *flexion){
   return flexion[INDEX_IND]<=(ANALOG_MAX/2)?0:1;
 }
