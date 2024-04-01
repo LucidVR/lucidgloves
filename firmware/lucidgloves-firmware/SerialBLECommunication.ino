ff001a6a409fd0709990f9ce5c0b02b58dcf7dda
DieKatzchen <himitsu73@hotmail.com>
2024-04-01 19:00:50 -0400

    Add notify()
    Somehow this got lost between versions of the code

--- firmware/lucidgloves-firmware/SerialBLECommunication.ino
+++ firmware/lucidgloves-firmware/SerialBLECommunication.ino
@@ -43,6 +43,7 @@ class BLESerialCommunication : public ICommunication {
             NimBLECharacteristic* qChr = pSvc->getCharacteristic("6E400003-B5A3-F393-E0A9-E50E24DCCA9E");
             if(qChr) {
                 qChr->setValue(String(data));
+                qChr->notify();
             }
         }
       }
